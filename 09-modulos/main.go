32c81618f4d99a16d3de2c6207659de72e7aa791
Juan Serna <juandavidserna12@gmail.com>
2022-05-18 00:34:29 -0500

    Esta es una actualizacion e inicio de creacion de modulos en Golang

---
+++ 09-modulos/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mariomac/analizador"
+	"juan.info/go/ejemplo/hola"
+)
+
+func main() {
+	fmt.Print("Como te llamas?: ")
+	var nombre string
+	fmt.Scanln(&nombre)
+	fmt.Println(hola.ConNombre(nombre))
+	
+	analizador.PrintEstadistica(nombre)
+}
